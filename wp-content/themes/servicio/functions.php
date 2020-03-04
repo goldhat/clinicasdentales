1f380929262eed389ffc88b54bad9d1e33234ee0
Casey Milne <joel@goldhat.ca>
2020-03-04 03:42:27 -0500

    enq styles, fix directory image

--- wp-content/themes/servicio/functions.php
+++ wp-content/themes/servicio/functions.php
@@ -1,7 +1,15 @@
 <?php
 
-add_action( 'after_setup_theme', 'register_my_menu' );
-
-function register_my_menu() {
+add_action( 'after_setup_theme', 'servicioMenu' );
+function servicioMenu() {
   register_nav_menu( 'primary', __( 'Primary Menu', 'servicio' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'servicioScripts' );
+function servicioScripts() {
+  wp_enqueue_style(
+    'servicio-style',
+    get_template_directory_uri() . '/style.css',
+    false
+  );
 }
